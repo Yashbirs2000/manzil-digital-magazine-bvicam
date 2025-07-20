eadff925740b1144ca20dd149d1674a2fce9bae6
Yashbirs2000 <yashbirs2000@gmail.com>
2025-07-21 00:13:05 +0530

    Update middlewares.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,32 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ]; 
 module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://manzil-digital-magazine-bvicam-j7hx.vercel.app',
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-]; 
+];
