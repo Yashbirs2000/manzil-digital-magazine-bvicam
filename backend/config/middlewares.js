e971c116eef11b1b21b0b88e75d0da209b8c56b7
Yashbirs2000 <yashbirs2000@gmail.com>
2025-07-20 12:42:58 +0530

    Update middlewares.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,22 +2,11 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:5173', // Vite local dev
-        'https://manzil-digital-magazine-bvicam-j7hx-od4pe4sf4.vercel.app/', // Replace with your deployed frontend domain
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      headers: '*',
-      credentials: true
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]; 
